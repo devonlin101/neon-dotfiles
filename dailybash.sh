b1cb217954c8b69c0ea6dd98dcebfc9b0c0fab00
devonlin101 <devonlin101@gmail.com>
2025-07-03 14:38:38 -0400

    update dailybash.sh file config fastfetch

--- dailybash.sh
+++ dailybash.sh
@@ -1,12 +1,12 @@
-sudo pkcon refresh;
-sudo pkcon update;
-sudo ubuntu-drivers autoinstall;
-npm outdated -g;
-npm update -g;
-zinit self-update;
-zinit update;
-sudo apt-get clean -y;
-sudo apt-get autoclean -y;
-sudo apt-get autoremove -y;
+sudo pkcon refresh
+sudo pkcon update
+sudo ubuntu-drivers autoinstall
+npm outdated -g
+npm update -g
+zinit self-update
+zinit update
+sudo apt-get clean -y
+sudo apt-get autoclean -y
+sudo apt-get autoremove -y
 date
-fastfetch
+fastfetch --config groups
