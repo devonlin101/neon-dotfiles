9c4753c8add6d59c934494417e70f9630f19c83b
devonlin101 <devonlin101@gmail.com>
2025-07-08 17:40:32 -0400

    update dailybash.sh

--- dailybash.sh
+++ dailybash.sh
@@ -3,10 +3,9 @@ sudo pkcon update
 sudo ubuntu-drivers autoinstall
 # pnpm outdated -g
 # pnpm update -g
-zinit self-update
-zinit update
 sudo apt-get clean -y
 sudo apt-get autoclean -y
 sudo apt-get autoremove -y
 date
 fastfetch --config os
+fastfetch
